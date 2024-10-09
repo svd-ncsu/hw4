1b9f02f4e73b9c1b5993af76720efc3b0ada96be
Devanshu Shah <92639289+devanshu-777@users.noreply.github.com>
2024-10-09 19:49:25 +0000

    Updated task3.sh

--- task3.sh
+++ task3.sh
@@ -2,9 +2,16 @@
 
 gawk -F, 'BEGIN { OFS="," }
 {
-  if ($3 == 2 && $13 ~ /S/ && $7 != "" && $7 ~ /^[0-9]+(\.[0-9]+)?$/) {
+  if ($3 == 2 && $13 ~ /S/) {
     gsub(/female/, "F", $0);
     gsub(/male/, "M", $0);
     print $0;
   }
-}' titanic.csv | tee /dev/tty | gawk -F, '{ sum += $7; count+=1 } END { if (count > 0) print "Average Age: " sum/count; else print "No matching records" }'
+}' titanic.csv | tee /dev/tty | gawk -F, '
+{
+  if($7 != "" && $7 ~ /^[0-9]+(\.[0-9]+)?$/) {
+    sum += $7; count+=1
+  }
+}
+END { if (count > 0) print "Average Age: " sum/count; else print "No matching records" 
+}'
